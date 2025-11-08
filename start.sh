bce2ab61299166d8d3a10bb0baa0cc4f2ed77a7c
mstanssa <mstanssa@gmail.com>
2025-11-08 14:56:02 +0000

    Fix ffmpeg install for Render

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+# تثبيت ffmpeg وقت التشغيل فقط
+if ! command -v ffmpeg &> /dev/null
+then
+  echo "Installing ffmpeg..."
+  apt-get update -y && apt-get install -y ffmpeg
+fi
+
 python3 bot.py
